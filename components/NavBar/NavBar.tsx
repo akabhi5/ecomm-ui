89f45ca25ec8766aa1861403568c8b1b1a7b78d3
Abhishek Kumar <abhishekk580@gmail.com>
2023-11-12 13:55:10 +0530

    Navbar

---
+++ components/NavBar/NavBar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const NavBar = () => {
+  return (
+    <nav className="p-5 bg-cyan-400 flex justify-between shadow-xl">
+      <div className="text-2xl font-bold">
+        <Link href="/">Ecomm</Link>
+      </div>
+      <div>
+        <ul className="flex space-x-5">
+          <li>Categories</li>
+          <li>User</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
