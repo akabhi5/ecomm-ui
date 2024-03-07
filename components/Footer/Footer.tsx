590b4e8a50191bef7b14d6da93226ee5b0eb483a
Abhishek Kumar <abhishekk580@gmail.com>
2024-03-07 11:31:29 +0530

    backend tech added

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,16 +1,24 @@
 const Footer = () => {
   return (
     <footer className="footer p-10 bg-base-200 text-base-content flex justify-between items-center bg-slate-100">
-      <aside>
-        <p>Tech used to build this app</p>
-
+      <aside className="flex space-x-5 divide-x-2">
         <div>
+          <p>Tech used to build this app</p>
           <ul className="list-disc ml-10">
             <li>Next.js</li>
             <li>Zustand</li>
             <li>TainwindCSS</li>
             <li>Shadcn UI</li>
             <li>React Hook Form</li>
+          </ul>
+        </div>
+
+        <div className="pl-5">
+          <p>Backend</p>
+          <ul className="list-disc ml-10">
+            <li>Django</li>
+            <li>PostgreSQL</li>
+            <li>Django REST Framework</li>
           </ul>
         </div>
       </aside>
